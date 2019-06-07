c0f007ee7ea083073c50edf15e5737de75fecb7a
1049259456@qq.com <1049259456@qq.com>
2019-06-07 15:20:27 +0800

    build webpack

---
+++ lib/index.tsx
@@ -0,0 +1,4 @@
+import Button from './button'
+import React from 'react';
+import ReactDom from 'react-dom';
+ReactDom.render(<div><Button/></div>,document.body);
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+    mode: 'production',
+    entry: './lib/index.tsx',
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx']
+    },
+    output: {
+        path: path.resolve(__dirname,'dist/lib'),
+        library: 'FUI',
+        libraryTarget: 'umd'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'awesome-typescript-loader'
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'index.html'
+        })
+    ],
+    externals: {
+        react: {
+            commonjs: 'react',
+            commonjs2: 'react',
+            amd: 'react',
+            root: 'React'
+        },
+        'react-dom': {
+            commonjs: 'react-dom',
+            commonjs2: 'react-dom',
+            amd: 'react-dom',
+            root: 'ReactDom'
+        }
+    }
+};
