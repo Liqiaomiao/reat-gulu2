33c600900f77de380ef15a6a95c720979046d585
Liqiaomiao <1049259456@qq.com>
2019-06-09 21:31:42 +0800

    webpack 整理

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,11 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-    // mode: 'production',
     entry: './lib/index.tsx',
     resolve: {
         extensions: ['.ts', '.tsx', '.js', '.jsx']
     },
     output: {
+        filename: "index.js",
         path: path.resolve(__dirname,'dist/lib'),
         library: 'FUI',
         libraryTarget: 'umd'
@@ -18,24 +17,5 @@ module.exports = {
                 loader: 'awesome-typescript-loader'
             }
         ]
-    },
-    // plugins: [
-    //     new HtmlWebpackPlugin({
-    //         template: 'index.html'
-    //     })
-    // ],
-    // externals: {
-    //     react: {
-    //         commonjs: 'react',
-    //         commonjs2: 'react',
-    //         amd: 'react',
-    //         root: 'React'
-    //     },
-    //     'react-dom': {
-    //         commonjs: 'react-dom',
-    //         commonjs2: 'react-dom',
-    //         amd: 'react-dom',
-    //         root: 'ReactDom'
-    //     }
-    // }
+    }
 };
