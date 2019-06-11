f2bbc22b2958cbcaba84185cb6eae0e6868b70a8
Liqiaomiao <1049259456@qq.com>
2019-06-11 23:02:00 +0800

    refactor: webpack.config.js 注释整理

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,5 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-    // mode: 'production',
     entry: './lib/index.tsx',
     resolve: {
         extensions: ['.ts', '.tsx', '.js', '.jsx']
@@ -18,24 +16,5 @@ module.exports = {
                 loader: 'awesome-typescript-loader'
             }
         ]
-    },
-    // plugins: [
-    //     new HtmlWebpackPlugin({
-    //         template: 'index.html'
-    //     })
-    // ],
-    // externals: {
-    //     react: {
-    //         commonjs: 'react',
-    //         commonjs2: 'react',
-    //         amd: 'react',
-    //         root: 'React'
-    //     },
-    //     'react-dom': {
-    //         commonjs: 'react-dom',
-    //         commonjs2: 'react-dom',
-    //         amd: 'react-dom',
-    //         root: 'ReactDom'
-    //     }
-    // }
+    }
 };
