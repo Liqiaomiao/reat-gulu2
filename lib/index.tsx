c6dd6525c28f47af73e279789054d34a21500897
Liqiaomiao <1049259456@qq.com>
2019-06-12 00:03:27 +0800

    feat: 创建icon.tsx

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,4 +1,4 @@
-import Button from './button'
 import React from 'react';
 import ReactDom from 'react-dom';
-ReactDom.render(<div><Button/></div>,document.body);
+import Icon from './icon';
+ReactDom.render(<div><Icon name="wechat"/></div>,document.body);
