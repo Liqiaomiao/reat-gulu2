f72183216d223cbc7633462325bc5cc90517beee
Liqiaomiao <1049259456@qq.com>
2019-06-14 00:28:44 +0800

    feat: 将多个svg同时导入，并且根据传递的name不同展示不同的svg图标

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,4 +1,9 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import Icon from './icon';
-ReactDom.render(<div><Icon name="wechat"/></div>,document.getElementById('root'));
+
+ReactDom.render(<div>
+        <Icon name="wechat"/>
+        <Icon name="alipay"/>
+    </div>,
+    document.getElementById('root'));
