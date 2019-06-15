7e4f308fd3e50718c73aad90cb800f49b006cacf
Liqiaomiao <1049259456@qq.com>
2019-06-15 11:01:58 +0800

    feat: icon增加点击事件

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './importicons'
 import './icon.scss'
 interface IconProps {
-    name: string
+    name: string,
+    onClick: React.MouseEventHandler<SVGElement>
+   // onClick: (e:React.MouseEvent)=> void
 }
 const Icon:React.FunctionComponent<IconProps> = (props)=>{
     return(
         <span>
-            <svg className='gulu-icon'>
+            <svg className='gulu-icon' onClick={props.onClick} >
                 <use xlinkHref={`#${props.name}`}/>
             </svg>
         </span>
--- lib/index.tsx
+++ lib/index.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import Icon from './icon';
+const fn:React.MouseEventHandler = (e) =>{
+    console.log(e);
+    console.log(e.target);
 
+};
+// const fn = (e:React.MouseEvent) =>{
+//     console.log(e);
+//     console.log(e.target);
+//
+// };
 ReactDom.render(<div>
-        <Icon name="wechat"/>
-        <Icon name="alipay"/>
+        <Icon name="wechat" onClick = {fn}/>
     </div>,
     document.getElementById('root'));
