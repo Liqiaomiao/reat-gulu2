d5de308e8b4d0b809ef5358ec9b1b0834f5c6025
Liqiaomiao <1049259456@qq.com>
2019-06-26 23:46:05 +0800

    feat: 增加onclick测试

--- lib/icon/__tests__/icon.unit.jsx
+++ lib/icon/__tests__/icon.unit.jsx
@@ -1,10 +1,20 @@
 import * as renderer from 'react-test-renderer'
 import React from 'react'
 import Icon from '../icon'
-
+import { mount } from 'enzyme'
 describe('icon', () => {
     it('xxx',() => {
         const json = renderer.create(<Icon name={'wechat'}/>).toJSON();
         expect(json).toMatchSnapshot()
+    })
+    it('onclick',()=>{
+        let n = 1;
+        const fn = ()=> {
+            n =2
+        }
+        const component = mount(<Icon name={'alipay'} onClick={fn}/>);
+        console.log(component.find('svg'));
+        component.find('svg').simulate('click');
+        expect(n).toEqual(2)
     })
 });
