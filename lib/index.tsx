d006b2dcea80ed20c4799ed3de33e5978139ca30
Liqiaomiao <1049259456@qq.com>
2019-07-05 23:06:20 +0800

    feat: 持续集成增加publish add token

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,17 +1,1 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import Icon from './icon/icon';
-const fn:React.MouseEventHandler = (e) =>{
-    console.log(e);
-    console.log(e.target);
-    console.log((e.target as SVGUseElement).href);
-};
-// const fn = (e:React.MouseEvent) =>{
-//     console.log(e);
-//     console.log(e.target);
-//
-// };
-ReactDom.render(<div>
-        <Icon className="wechat" name="wechat" onClick = {fn}/>
-    </div>,
-    document.getElementById('root'));
+export { default as Icon }  from './icon/icon';
