e08ef5811afd9b0296a9eeae4a0895965fd984e0
Liqiaomiao <1049259456@qq.com>
2019-07-31 22:05:01 +0800

    提交example

---
+++ lib/button/button.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const Button:React.FunctionComponent= ()=>{
+    return(
+        <span>
+          <button>click</button>
+        </span>
+    )
+};
+export default Button
