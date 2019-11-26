4af3db07675c0d3183fe1489691c418aed877fd3
Liqiaomiao <1049259456@qq.com>
2019-11-26 09:14:37 +0800

    重构scopedClassMaker-将传来的字符串整合成需要的对象形式

--- lib/helpers/classes.tsx
+++ lib/helpers/classes.tsx
@@ -1,4 +1,4 @@
-function classes(...names:(string|undefined)[]) {
+function classes(...names: (string | undefined)[]) {
     return names.filter(Boolean).join(' ')
 }
 export default classes
@@ -13,24 +13,23 @@ function scopedClassMaker(prefix:string) {
     return function (name:string| ClassToggles ,options?:Props) {
 
         let name2
-        if (typeof name === 'string' || name === undefined) {
-            name2 =  [prefix,name].filter(Boolean).join('-')
-        }else{
-            // {active:true,current:true,hello:false}
-            name2 = Object.entries(name) // [[active,true],[current,true],[hello,false]]
-                .filter(kv=>kv[1]  === true) // [[active,true],[current,true]]
-                .map(kv=>kv[0]) // [active,current]
-                .map(name=>[prefix,name].filter(Boolean).join('-')) // [gulu-layout-active,gulu-layout-curren
-                .join(' ')
+        if (typeof name === 'string') {
+            name2 = {[name]: true}
+        } else {
+            name2 = name
         }
-
-
-        if(options && options.extra){
-            return [name2,options && options.extra].filter(Boolean).join(' ');
-        }else{
+         name2 = Object.entries(name2) // [[active,true],[current,true],[hello,false]]
+            .filter(kv => kv[1] === true) // [[active,true],[current,true]]
+            .map(kv => kv[0])
+            .map(name => [prefix, name].filter(Boolean).join('-'))// [gulu-layout-active,gulu-layout-curren
+            .join(' ')
+        if (options && options.extra) {
+            return [name2, options && options.extra].filter(Boolean).join(' ');
+        } else {
             return name2;
         }
 
     }
 }
+
 export {scopedClassMaker}
