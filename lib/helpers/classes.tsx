ae6ecfe1ab048a9f98e2c8f531c0767f458ab317
Liqiaomiao <1049259456@qq.com>
2019-11-26 22:52:31 +0800

    重构scopedClassMaker~三目运算

--- lib/helpers/classes.tsx
+++ lib/helpers/classes.tsx
@@ -15,23 +15,18 @@ interface ClassToggles {
 function scopedClassMaker(prefix: string) {
     return function (name: string | ClassToggles, options?: Props) {
 
-        let name2
-        if (typeof name === 'string') {
-            name2 = {[name]: true}
-        } else {
-            name2 = name
-        }
-        name2 = Object.entries(name2) // [[active,true],[current,true],[hello,false]]
+        let namesObject = (typeof name === 'string') ?
+            {[name]: true} :
+            name
+
+        let scoped = Object.entries(namesObject) // [[active,true],[current,true],[hello,false]]
             .filter(kv => kv[1] === true) // [[active,true],[current,true]]
             .map(kv => kv[0])
             .map(name => [prefix, name].filter(Boolean).join('-'))// [gulu-layout-active,gulu-layout-curren
             .join(' ')
-        if (options && options.extra) {
-            return [name2, options && options.extra].filter(Boolean).join(' ');
-        } else {
-            return name2;
-        }
-
+        return (options && options.extra) ?
+            [scoped, options && options.extra].filter(Boolean).join(' '):
+            scoped
     }
 }
 
