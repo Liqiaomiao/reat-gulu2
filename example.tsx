f7836b9f1d248122417a9bdc6e545f30f9f77968
Liqiaomiao <1049259456@qq.com>
2019-11-27 08:40:13 +0800

    使用自己的layout重构自己的官网

--- example.tsx
+++ example.tsx
@@ -4,42 +4,45 @@ import {HashRouter as Router, Route, Link} from "react-router-dom";
 import IconExample from './lib/icon/icon.example'
 import ButtonExample from './lib/button/button.example'
 import Dialog from "./lib/dialog/dialog.example"
-import Layout from "./lib/layout/layout.example"
-
+import LayoutExample from './lib/layout/layout.example'
+import {Aside, Header, Layout, Content} from "./lib/layout/layout"
+import './example.scss'
 ReactDom.render((
-    <Router>
-        <div>
-            <header>
-                <div className="logo">
-                    LiUI
-                </div>
-            </header>
-            <div>
-                <aside>
-                    <h2>组件</h2>
-                    <ul>
-                        <li>
-                            <Link to='/icon'>Icon</Link>
-                        </li>
-                        <li>
-                            <Link to='/button'>Button</Link>
-                        </li>
-                        <li>
-                            <Link to='/dialog'>dialog</Link>
-                        </li>
-                        <li>
-                            <Link to='/layout'>layout</Link>
-                        </li>
-                    </ul>
-                </aside>
-                <main>
-                    <Route path="/icon" component={IconExample}/>
-                    <Route path="/button" component={ButtonExample}/>
-                    <Route path="/dialog" component={Dialog}/>
-                    <Route path="/layout" component={Layout}/>
-                </main>
-            </div>
-        </div>
-    </Router>
+        <Router>
+            <Layout className={'page'}>
+                <Header>
+                    <div className="logo">
+                        LiUI
+                    </div>
+                </Header>
+                <Layout>
+                    <Aside>
+                        <h2>组件</h2>
+                        <ul>
+                            <li>
+                                <Link to='/icon'>Icon</Link>
+                            </li>
+                            <li>
+                                <Link to='/button'>Button</Link>
+                            </li>
+                            <li>
+                                <Link to='/dialog'>dialog</Link>
+                            </li>
+                            <li>
+                                <Link to='/layout'>layout</Link>
+                            </li>
+                        </ul>
+                    </Aside>
+                    <Content>
+                        <main>
+                            <Route path="/icon" component={IconExample}/>
+                            <Route path="/button" component={ButtonExample}/>
+                            <Route path="/dialog" component={Dialog}/>
+                            <Route path="/layout" component={LayoutExample}/>
+                        </main>
+                    </Content>
+                </Layout>
+            </Layout>
+        </Router>
     ), document.querySelector('#root')
 );
