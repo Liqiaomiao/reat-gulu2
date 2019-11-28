dc5f26b6cb23b6ab110f282e8cbf423e1f2b7b21
Liqiaomiao <1049259456@qq.com>
2019-11-28 23:09:10 +0800

    prism-react-renderer 增加代码高亮，代码显示/代码隐藏

--- lib/icon/demo.tsx
+++ lib/icon/demo.tsx
@@ -1,16 +1,33 @@
-import React from 'react';
-
+import React,{useState} from 'react';
+import Highlight, { defaultProps } from "prism-react-renderer";
 interface DemoProps{
     code: {default:string}
 }
 
 const Demo:React.FunctionComponent<DemoProps> = (props)=>{
+    const [codeVisible, setCodeVisible] = useState(false)
+    const code = (
+        <Highlight {...defaultProps} code={props.code.default} language="jsx">
+            {({className, style, tokens, getLineProps, getTokenProps}) => (
+                <pre className={className} style={style}>
+        {tokens.map((line, i) => (
+            <div {...getLineProps({line, key: i})}>
+                {line.map((token, key) => (
+                    <span {...getTokenProps({token, key})} />
+                ))}
+            </div>
+        ))}
+      </pre>
+            )}
+        </Highlight>
+    )
     return(
         <div>
-            {props.children}
-            <pre>
-                {props.code.default}
-            </pre>
+            <div>
+                {props.children}
+                <button className='code-toggle' onClick={()=>setCodeVisible(!codeVisible)}>{codeVisible?'隐藏代码':'显示代码'}</button>
+            </div>
+            {codeVisible && code}
         </div>
     )
 };
