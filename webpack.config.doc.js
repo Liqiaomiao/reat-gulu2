ebacdf693977a4d06b977c6500e6529a71b5a898
Liqiaomiao <1049259456@qq.com>
2019-11-29 08:44:20 +0800

    webpack 生成doc文件夹（build 后的内容）

---
+++ webpack.config.doc.js
@@ -0,0 +1,16 @@
+const base = require('./webpack.config');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = Object.assign({},base,{
+    mode: 'production',
+    entry: './example.tsx',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'example.html',
+            filename: 'example.html'
+        })
+    ],
+    output: {
+        path: path.resolve(__dirname,'doc')
+    },
+});
