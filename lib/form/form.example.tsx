2d7f54e03d9ac3396e79d989c5557b5eeff3e491
Liqiaomiao <1049259456@qq.com>
2019-12-01 18:10:52 +0800

    代码格式化

--- lib/form/form.example.tsx
+++ lib/form/form.example.tsx
@@ -1,38 +1,39 @@
 import React, {useState, Fragment} from "react";
-import Form,{FormValue} from "./form";
+import Form, {FormValue} from "./form";
 import Validator from "./validator";
-const FormExample:React.FunctionComponent = ()=>{
+
+const FormExample: React.FunctionComponent = () => {
     const [formData, setFormData] = useState<FormValue>({
-        username:'andy',
-        password:'12345'
+        username: 'andy',
+        password: '12345'
     })
     const [errors, setErrors] = useState({})
     const [fields] = useState([
-        {name:'username',label:'用户名',input:{type:'text'}},
-        {name:'password',label:'密码',input:{type:'password'}}
+        {name: 'username', label: '用户名', input: {type: 'text'}},
+        {name: 'password', label: '密码', input: {type: 'password'}}
     ])
-    const onsubmit = (e:React.FormEvent)=>{
-      const formRules=[
-          {key:'username',required:true},
-          {key:'username',minLength: 3,maxLength: 5},
-          {key:'password',minLength: 3,pattern:/[A-Za-z0-9]+/}
-      ]
-      const errors = Validator(formData,formRules)
+    const onsubmit = (e: React.FormEvent) => {
+        const formRules = [
+            {key: 'username', required: true},
+            {key: 'username', minLength: 3, maxLength: 5},
+            {key: 'password', minLength: 3, pattern: /[A-Za-z0-9]+/}
+        ]
+        const errors = Validator(formData, formRules)
         setErrors(errors)
     }
     return (
         <div>
-            <Form value={formData} fields = {fields} buttons={
+            <Form value={formData} fields={fields} buttons={
                 (<Fragment>
                     <button type='submit'>确定</button>
                     <button>取消</button>
                 </Fragment>)
             } onSubmit={onsubmit}
-              errors={errors}
-              onChange={(newValue)=>setFormData(newValue)}
+                  errors={errors}
+                  onChange={(newValue) => setFormData(newValue)}
             >
             </Form>
         </div>
     )
 }
-export  default FormExample;
+export default FormExample;
--- lib/form/form.tsx
+++ lib/form/form.tsx
@@ -1,33 +1,36 @@
 import React, {ReactFragment} from "react";
-export interface FormValue{
-    [T:string]:any
+
+export interface FormValue {
+    [T: string]: any
 }
+
 interface Props {
     value: FormValue,
-    fields: Array<{name:string,label:string,input:{type:string}}>,
-    buttons:ReactFragment,
+    fields: Array<{ name: string, label: string, input: { type: string } }>,
+    buttons: ReactFragment,
     onSubmit: React.FormEventHandler<HTMLFormElement>,
-    onChange: (value:FormValue)=> void,
-    errors:{[K:string]:string[]}
+    onChange: (value: FormValue) => void,
+    errors: { [K: string]: string[] }
 }
-const Form:React.FunctionComponent<Props> = (props)=>{
-    const onsubmit:React.FormEventHandler<HTMLFormElement> = (e)=>{
+
+const Form: React.FunctionComponent<Props> = (props) => {
+    const onsubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
         e.preventDefault()
         props.onSubmit(e)
     }
     const formData = props.value
-    const onchange=(a:string,e:React.ChangeEvent<HTMLInputElement>)=>{
-       props.onChange({...formData,[a]:e.target.value})
+    const onchange = (a: string, e: React.ChangeEvent<HTMLInputElement>) => {
+        props.onChange({...formData, [a]: e.target.value})
     }
     return (
         <form onSubmit={onsubmit}>
-            {props.fields.map(item=>(
+            {props.fields.map(item => (
                 <div key={item.name}>
                     {item.label}
                     <input type={item.input.type}
                            name={item.name}
                            value={formData[item.name]}
-                           onChange={onchange.bind(null,item.name)}/>
+                           onChange={onchange.bind(null, item.name)}/>
                     <div>{props.errors[item.name]}</div>
                 </div>
             ))}
